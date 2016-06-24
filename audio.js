90373f0b71b32dade9d3b8e43415c4c594bcac51
Benjamin Dant <benjamindant@gmail.com>
2016-06-24 13:20:12 +0200

    added loading of external files

--- audio.js
+++ audio.js
@@ -1,6 +1,7 @@
-var playlist = [
+var defaultPlaylist = [
+	"02 恋愛♥ライダー.m4a",
 	"10 Cliffs of Dover.m4a",
-	"10 Lottara Lottara.m4a",
+	"10 ロッタラ ロッタラ.m4a",
 	"Far from love.mp3",
 	"Globalization.mp3",
 	"Journey.mp3",
@@ -22,36 +23,47 @@ var playlist = [
 	"Thee Emergency  Total Energy.MP3",
 	"Trailer77.mp3",
 ];
+var playlist = [];
+if(self.fetch){
+	fetch('./playlist.json').then(function(datas){
+		datas.json().then(function(json){
+			playlist = json.list;
+			console.log(playlist);
+		})
+	});
+} else playlist = defaultPlaylist;
 var current = 0;
 var audio;
 var title;
 
 window.onload = function(){
-	audio = document.getElementById('audio');
-	title = document.getElementById('title');
-	title.innerHTML = playlist[0];
-	audio.onload = function(){
-		console.log('Loading File');
-	}
-	audio.src = "audio/" + playlist[0];
-	document.onkeydown = function (e) {
-	    'use strict';
-	    console.log('Key Code', e.keyCode);
-	    console.log('Ended ?', audio.ended)
-	    if (e.keyCode === 83) {
-			audio.pause();
-		} else if (e.keyCode === 80) {
-			audio.play();
-		} else if (e.keyCode === 78) {
-			Next(audio);
+	setTimeout(function(){
+		audio = document.getElementById('audio');
+		title = document.getElementById('title');
+		title.innerHTML = playlist[0];
+		audio.onload = function(){
+			console.log('Loading File');
 		}
-	}
-	var autoplay = setInterval(function(){
-		if (audio.ended) {
-			console.log('Ended');
-			Next();
+		audio.src = "audio/" + playlist[0];
+		document.onkeydown = function (e) {
+		    'use strict';
+		    console.log('Key Code', e.keyCode);
+		    console.log('Ended ?', audio.ended)
+		    if (e.keyCode === 83) {
+				audio.pause();
+			} else if (e.keyCode === 80) {
+				audio.play();
+			} else if (e.keyCode === 78) {
+				Next(audio);
+			}
 		}
-	}, 1000);
+		var autoplay = setInterval(function(){
+			if (audio.ended) {
+				console.log('Ended');
+				Next();
+			}
+		}, 1000);
+	}, 500);
 };
 
 var Next = function(){
