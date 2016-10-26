a7f62af062c207fcd62984a2a68ab9ab1c46eba6
Benjamin Dant <benjamindant@gmail.com>
2016-10-26 12:11:14 +0200

    Modified html file to php

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title id="onglet">HTML5 Game in Dev</title>
+	<meta charset="UTF-8">
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="shortcut icon" type="image/png" href="http://game.dev/favicon.png">
+</head>
+<body>
+<h1>Introduction</h1>
+<canvas id="game"></canvas>
+<h5><span id="title"></span>
+<audio controls id="audio">
+	<source src="" type=""/>
+</audio></h5>
+<section id="main">
+<p>Août</p>
+	īe&#772;
+</section>
+<?php echo 'Helllo World';?>
+<script type="text/javascript" src="canvas.js"></script>
+<script type="text/javascript" src="audio.js" charset="UTF-8"></script>
+</body>
+</html>
